f9591909aeef24e59b29cb0beccca82b31b2bc48
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-02-25 20:11:38 -0700

    Got LightHouse Request Working

--- app.js
+++ app.js
@@ -1,68 +1,52 @@
-// var express = require('express')
-// var app = express()
-
-// var lastUserId = 2;
-//
-// var users = [
-//  {
-//    id: 1,
-//    name: 'Mike',
-//    age: 21,
-//  },{
-//    id: 2,
-//    name: 'Nancy',
-//    age: 20,
-//  }
-// ];
-
-//
-// app.get('/user/:userId', function (req, res) {
-//   var userId = parseInt(req.params.userId);
-//
-//   var userName = 'Can\'t find user';
-//   users.forEach(function(user) {
-//     if (user.id === userId) {
-//       userName = user.name;
-//     }
-//   })
-//
-//   res.send(`Your username: ${userName}`)
-// })
-
-
-// app.post('/createuser', function(req, res) {
-//   var name = req.body.name
-//   var age = req.body.age
-//
-//   var newId = lastUserId + 1;
-//   lastUserId = newId;
-//
-//   var user = {
-//     id: newId,
-//     name: name,
-//     age: age,
-//   }
-//
-//   users.push(user)
-//
-//   res.send(200)
-// })
-//
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
-// })
 var database = firebase.database();
+// var admin = require('firebase-admin');
+// var db = admin.database();
+var refData = database.ref('users');
 
-function writeUserData(userId, name) {
-  firebase.database().ref('users/' + userId).set({
-    username: name
+function writeUserData(name, address, child) {
+  firebase.database().ref('/users/').once('value').then(function(snapshot) {
+    var userId;
+    if (snapshot.val()) {
+      userId = Object.keys(snapshot.val()).length + 1;
+      console.log('number of users in database:', Object.keys(snapshot.val()).length)
+      console.log('adding user with userId:', Object.keys(snapshot.val()).length + 1)
+    }
+    else {
+      userId = 1;
+      console.log('no users in database, setting userId to 1')
+    }
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      user_address: address,
+      user_children: child
+
+    });
   });
 }
 
-// document.querySelector('.clickme').addEventListener('click', function() {
-//   writeUserData(2, $('#username').val())
-// })
+
 $('.clickme').click(function() {
-  writeUserData(2, $('#username').val())
-})
+  writeUserData($('#hostName').val(), $('#hostAddress').val(), $('#children').val());
+});
+
+
+$('.requestHouse').click(function() {
+var childrenOkay = $('#children').val();
+console.log(childrenOkay)
+
+  refData.on("value",function(snapshot){
+    // console.log(snapshot.val());
+    var userHouseData = snapshot.val();
+    for (var i =0; i < userHouseData.length; i ++) {
+      console.log(userHouseData)
+      if(userHouseData[i] !== undefined){
+      if (userHouseData[i].user_children === childrenOkay){
+        console.log("were at the if statement")
+        console.log(userHouseData[i].user_children)
+      }
+    }
+  }
+}, function (errorObject){
+    console.log("the read failed:" + errorObject.code);
+  });
+});
